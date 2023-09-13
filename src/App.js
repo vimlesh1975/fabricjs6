dcdef2fae87b625146172ed0e16100fc82b65a12
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-09-13 10:25:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import * as fabric from 'fabric';
 
 function App() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      backgroundColor: 'white',
+      width: 1920,
+      height: 1080,
+    });
+
+    const textbox = new fabric.Textbox('Enter your text', {
+      left: 100,
+      top: 100,
+      width: 800,
+      fontSize: 120,
+      borderColor: 'black',
+      cornerColor: 'black',
+      cornerSize: 6,
+      transparentCorners: false,
+      stroke: 'red',
+      strokeWidth: 3,
+      fill: 'yellow',
+    });
+
+    canvas.add(textbox);
+    canvas.renderAll();
+
+    return () => {
+      canvas.dispose();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas ref={canvasRef} />
     </div>
   );
 }
